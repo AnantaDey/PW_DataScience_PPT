4b4f774452d63210c05526a700b73a4b09e14e83
AnantaDey <125046645+AnantaDey@users.noreply.github.com>
2023-06-25 11:47:23 +0530

    Create MoveZeros.cpp

---
+++ Mock Test 1/MoveZeros.cpp
@@ -0,0 +1,17 @@
+void moveZeroes(vector<int>& nums) {
+        //initializing from first
+        int i=0;
+
+        //iterating from 0th index
+        for(int j=0;j<nums.size();j++)
+        {
+            //checking if nums[j] is other than zero
+            // then swap and increse the value of i
+            if(nums[j]!=0)
+            {
+                swap(nums[j],nums[i]);
+                i++;
+            }
+        }
+        
+    }
