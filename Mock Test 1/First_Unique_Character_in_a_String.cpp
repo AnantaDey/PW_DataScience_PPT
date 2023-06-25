f7d497c7241f58503d7904d2821f4f4d1e6be69e
AnantaDey <125046645+AnantaDey@users.noreply.github.com>
2023-06-25 12:05:12 +0530

    Update First_Unique_Character_in_a_String.cpp

--- Mock Test 1/First_Unique_Character_in_a_String.cpp
+++ Mock Test 1/First_Unique_Character_in_a_String.cpp
@@ -1,21 +1,37 @@
-int firstUniqChar(string s) {
+#include <bits/stdc++.h>
+using namespace std;
 
-  //I have taken unordered map to
-  //count the occurance of each character
-  //return the charcter whose count is one 
-  //and which comes first
-  unordered_map<char, int> mp;
-  
-  for (auto& c : s) ++mp[c];
-  
-  int n = s.size();
-  
-  for (int i = 0; i < n; ++i)
-    if (mp[s[i]] == 1)
-      return i;
-  return -1;
+int firstUniqChar(string s)
+{
+
+    // I have taken unordered map to
+    // count the occurance of each character
+    // return the charcter whose count is one
+    // and which comes first
+    unordered_map<char, int> mp;
+
+    for (auto &c : s)
+        ++mp[c];
+
+    int n = s.size();
+
+    for (int i = 0; i < n; ++i)
+        if (mp[s[i]] == 1)
+            return i;
+    return -1;
 }
 
+int main()
+{
+    string s1 = "leetcode";
+    string s2 = "loveleetcode";
+    string s3 = "aabb";
 
-//Time complexity O(n)
-//Space Complexity O(n)
+    cout << firstUniqChar(s1) << endl;
+    cout << firstUniqChar(s2) << endl;
+    cout << firstUniqChar(s3) << endl;
+    return 0;
+}
+
+// Time complexity O(n)
+// Space Complexity O(n)
